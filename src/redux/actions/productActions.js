d05754ff3e10549f7d37c76475e375adfdf6a7af
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-10 13:05:14 +0600

    selected product action type created

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -15,3 +15,12 @@ export const setProducts = (products) => {
 
     }
 }
+
+export const selectedProduct = (product) => {
+    return {
+
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload:product
+
+    }
+}
