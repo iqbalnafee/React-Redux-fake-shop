fe07b1b3368f55b8f6ce353469a86da8e9555fae
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-11 23:14:29 +0600

    routing added to the app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
 import './App.css';
-import Header from './components/Header'
-
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header />
+
+      <Router>
+
+        <Header />
+
+        
+        <Switch>
+
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+
+
+        </Switch>
+
+      </Router>
+
     </div>
   );
 }
