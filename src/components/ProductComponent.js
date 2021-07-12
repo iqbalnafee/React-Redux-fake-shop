7b6d5cf295370fbf9a97e8fc579429ac8ecc61d6
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-12 22:50:54 +0600

    rendered product listing page

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -4,20 +4,32 @@ import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
     const products = useSelector(state => state.allproducts.products);
-    // const {id,title,category} = products[0];
-
-    return (
-        <div className="four column wide">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image"></div>
-                    <div className="content">
-                        {/* <div className="header">{title}</div> */}
+    
+    const renderItems = products.map( (product) => {
+        const {id,title,price,description,category,image} = product;
+        return (
+            <div className="four wide column" key={id} style={{marginTop:"5%"}} >
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src = { image } alt="" />
+                        </div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        );
+    } );
+
+    return (
+        <>{renderItems} </>
     )
+
+   
 }
 
 export default ProductComponent
