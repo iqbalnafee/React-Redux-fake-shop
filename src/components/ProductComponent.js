c5e355dfdf443796ee75aee13bee67b1133bea75
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-12 22:04:50 +0600

    useSelector added to access our store

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
+    const products = useSelector(state => state.allproducts.products);
+    const {id,title,category} = products[0];
+
     return (
-        <div>
-            <h2>ProductComponent</h2>
+        <div className="four column wide">
+            <div className="ui link cards">
+                <div className="card">
+                    <div className="image"></div>
+                    <div className="content">
+                        <div className="header">{title}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React  from 'react';
+import { useSelector } from 'react-redux';
+import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
+
+    //we have to fetch the product that we have in our redux store
+    //In order to get the access in redux store we are gonna use useSelector
+
+    // React Redux offers a set of Hooks as an alternative to the existing connect() higher order component. These Hooks allow you to connect to the Redux store and dispatch actions without having to wrap your components in connect().
+
+    //The equivalent of map state to props is useSelector
+
+    //It takes in a function argument that returns the part of the state that you want
+
+    // const products = useSelector(state => state);
+    // console.log(products);
+
     return (
-        <div>
-            <h2>ProductListing</h2>
+        <div className="ui grid container">
+            <ProductComponent />
         </div>
     )
 }
